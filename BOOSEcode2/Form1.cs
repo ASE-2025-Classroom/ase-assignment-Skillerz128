a59ceedf7dd4684cb64a31127ee919d3e26b0949
rahulF <r.fahadh2573@student.leedsbeckett.ac.uk>
2025-10-17 17:03:18 +0100

    Set up

--- BOOSEcode2/Form1.cs
+++ BOOSEcode2/Form1.cs
@@ -1,3 +1,6 @@
+using BOOSE;
+using System.Diagnostics;
+
 namespace BOOSEcode2
 {
     public partial class Form1 : Form
@@ -5,6 +8,7 @@ namespace BOOSEcode2
         public Form1()
         {
             InitializeComponent();
+            Debug.WriteLine(AboutBOOSE.about());
         }
     }
 }
